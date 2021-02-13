3728ac89a8547e014e4e841f829a1a6a24493e5e
tdm <dtanukov@gmail.com>
2021-02-13 16:22:33 +0500

    Вторая домашняя работа

---
+++ lesson2/dz_2_1.py
@@ -0,0 +1,6 @@
+# Выяснить тип результата выражений:
+
+print(type(15 * 3))  #<class 'int'>
+print(type(15 / 3))  #<class 'float'>
+print(type(15 // 2))  #<class 'int'>
+print(type(15 ** 2))  #<class 'int'>
